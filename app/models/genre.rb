6cf2fb7ea204564fb664a5502dfcbb23d3d0c4b0
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-23 11:34:23 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     self.artists.pluck(:name)
-    #binding.pry
     # return an array of strings containing every musician's name
   end
 end
